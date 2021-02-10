684993eb28de79001da360ba7a7276b521babb35
Justin Patriquin <justinpatriquin@gmail.com>
2021-02-10 18:02:30 -0400

    fix accidental typo

--- examples/linear_regression/linear_regression_project.py
+++ examples/linear_regression/linear_regression_project.py
@@ -51,8 +51,8 @@ def setup_project():
     print('orgs', project.organizations)
 
     org_dv = {
-        project.organizations[0].name: 'y_data',
-        #project.organizations[1].name: 'y_data',
+        project.organizations[0].name: 'x_data',
+        project.organizations[1].name: 'y_data',
     }
 
     df = pd.DataFrame({"x": [1.0, 2.0], "y": [1.0, 2.0]})
