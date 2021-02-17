71a29229ec594d40ded59cf760b569ca00977cfa
Justin Patriquin <justinpatriquin@gmail.com>
2021-02-17 15:16:50 -0400

    update inits

---
+++ cape/api/job/__init__.py
@@ -0,0 +1,2 @@
+from .job import Job
+from .vertical_linear_regression_job import VerticalLinearRegressionJob
--- examples/linear_regression/linear_regression_project.py
+++ examples/linear_regression/linear_regression_project.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from cape.api.dataview import DataView
 from cape.api.job import VerticalLinearRegressionJob
-from cape.cape import Cape
+from cape.api.cape import Cape
 
 parser = argparse.ArgumentParser(description="Create Job for a given project ID plus other utilities")
 parser.add_argument("--token", default=os.environ.get("CAPE_TOKEN"))
